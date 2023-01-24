896350da47f2de4c2bcdf8c84a29cfaca8626db7
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2023-01-24 23:49:12 +0600

    solution

--- Array/1920_Build_Array_from_Permutation.cpp
+++ Array/1920_Build_Array_from_Permutation.cpp
@@ -1,1 +1,13 @@
 
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int len = nums.size();
+        vector<int>ans(len);
+        for(int i = 0;i<len;i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+        
+    }
+};
